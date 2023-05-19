394368d7507db5f7a7d01372756cbf894c9c8222
elisei <ge94jip@mytum.de>
2023-05-19 22:19:29 +0200

    q

--- src/gad/dynamicarray/DynamicArray.java
+++ src/gad/dynamicarray/DynamicArray.java
@@ -46,13 +46,11 @@ public class DynamicArray {
     }
 
     public int get(int index) {
-        assert(0 <= index && index < elements.length);
-        return this.elements[index];
+        return elements[index];
     }
 
     public void set(int index, int value) {
-        assert(0 <= index && index < elements.length);
-        this.elements[index] = value;
+        elements[index] = value;
     }
 
     public void reportArray(Result result) {
