714457e1b4827363c38ecaa4cb6810c26a901d63
artemis <artemis@xcit.tum.de>
2023-05-19 22:21:15 +0200

    Reset Exercise

--- src/gad/dynamicarray/DynamicArray.java
+++ src/gad/dynamicarray/DynamicArray.java
@@ -4,13 +4,8 @@ import java.util.Arrays;
 
 public class DynamicArray {
     private int[] elements;
-    private int growthFactor;
-    private  int maxOverhead;
 
-    public DynamicArray(int growthFactor, int maxOverhead){
-        if (maxOverhead < 1 || growthFactor < 1 || growthFactor > maxOverhead) throw new IllegalArgumentException();
-        this.growthFactor = growthFactor;
-        this.maxOverhead = maxOverhead;
+    public DynamicArray(int growthFactor, int maxOverhead) {
     }
 
     public int getLength() {
@@ -18,39 +13,14 @@ public class DynamicArray {
     }
 
     public Interval reportUsage(Interval usage, int minSize) {
-        if (minSize > elements.length || minSize * maxOverhead < elements.length ){
-            int[] mas = new int[minSize * growthFactor];
-            if (usage.isEmpty()){
-                elements = mas;
-                return Interval.EmptyInterval.getEmptyInterval();
-            }
-//            if (usage.getFrom() <= usage.getTo()){
-//                for(int i = usage.getFrom(); i <= usage.getTo(); i++){
-//                    mas[i - usage.getFrom()] = elements[i];
-//                }
-//                elements = mas;
-//                return new Interval.NonEmptyInterval(0,usage.getSize(elements.length));
-//            }
-//            else {
-//                for (int i = 0 ; i <= usage.getTo(); i++){
-//                    mas[elements.length - usage.getFrom() + i] = elements[i];
-//                }
-//                for(int i = 0; i + usage.getFrom() <= elements.length - 1; i++){
-//                    mas[i] = elements[usage.getFrom() + i];
-//                }
-//                elements = mas;
-//                return new Interval.NonEmptyInterval(0, elements.length - usage.getSize(elements.length) - 3);
-//            }
-        }
-        return usage;
+        return null;
     }
 
     public int get(int index) {
-        return elements[index];
+        return 0;
     }
 
     public void set(int index, int value) {
-        elements[index] = value;
     }
 
     public void reportArray(Result result) {
@@ -60,9 +30,5 @@ public class DynamicArray {
     @Override
     public String toString() {
         return Arrays.toString(elements);
-    }
-
-    public static void main(String[] args) {
-
     }
 }
