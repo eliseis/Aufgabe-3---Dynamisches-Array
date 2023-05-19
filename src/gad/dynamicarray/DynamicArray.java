5b012ba887ed9979103f0d7e65aac44aef49a6a2
elisei <ge94jip@mytum.de>
2023-05-19 22:32:06 +0200

    q

--- src/gad/dynamicarray/DynamicArray.java
+++ src/gad/dynamicarray/DynamicArray.java
@@ -4,8 +4,13 @@ import java.util.Arrays;
 
 public class DynamicArray {
     private int[] elements;
+    private int maxOverhead;
+    private int growthFactor;
 
     public DynamicArray(int growthFactor, int maxOverhead) {
+        if (maxOverhead <= 1 || growthFactor <= 1) throw new IllegalArgumentException();
+        this.growthFactor = growthFactor;
+        this.maxOverhead = maxOverhead;
     }
 
     public int getLength() {
@@ -13,7 +18,32 @@ public class DynamicArray {
     }
 
     public Interval reportUsage(Interval usage, int minSize) {
-        return null;
+
+        if (minSize > elements.length || minSize * maxOverhead < elements.length ){
+            int[] mas = new int[minSize * growthFactor];
+            if (usage.isEmpty()){
+                elements = mas;
+                return Interval.EmptyInterval.getEmptyInterval();
+            }
+            if (usage.getFrom() <= usage.getTo()){
+                for(int i = usage.getFrom(); i <= usage.getTo(); i++){
+                    mas[i - usage.getFrom()] = elements[i];
+                }
+                elements = mas;
+                return new Interval.NonEmptyInterval(0,usage.getSize(elements.length));
+            }
+            else {
+                for (int i = 0 ; i <= usage.getTo(); i++){
+                    mas[elements.length - usage.getFrom() + i] = elements[i];
+                }
+                for(int i = 0; i + usage.getFrom() <= elements.length - 1; i++){
+                    mas[i] = elements[usage.getFrom() + i];
+                }
+                elements = mas;
+                return new Interval.NonEmptyInterval(0, elements.length - usage.getSize(elements.length) - 3);
+            }
+        }
+        return usage;
     }
 
     public int get(int index) {
