086d0aa46dbd892bb497bc0dee5572d5a8184902
elisei <ge94jip@mytum.de>
2023-05-19 22:40:58 +0200

    q

--- src/gad/dynamicarray/DynamicArray.java
+++ src/gad/dynamicarray/DynamicArray.java
@@ -8,7 +8,7 @@ public class DynamicArray {
     private int growthFactor;
 
     public DynamicArray(int growthFactor, int maxOverhead) {
-        if (maxOverhead <= 1 || growthFactor <= 1 || ) throw new IllegalArgumentException();
+        if (maxOverhead <= 1 || growthFactor <= 1 || maxOverhead < growthFactor ) throw new IllegalArgumentException();
         this.growthFactor = growthFactor;
         this.maxOverhead = maxOverhead;
     }
