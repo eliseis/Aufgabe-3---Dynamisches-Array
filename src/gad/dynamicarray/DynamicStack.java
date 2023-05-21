8afffe7550f85b392cc9df5342cad82b194995b3
elisei <ge94jip@mytum.de>
2023-05-21 19:06:59 +0200

    q

--- src/gad/dynamicarray/DynamicStack.java
+++ src/gad/dynamicarray/DynamicStack.java
@@ -3,23 +3,42 @@ package gad.dynamicarray;
 public class DynamicStack implements Stack {
     private DynamicArray array;
     private Result result;
+    private int maxOverhead;
+    private int growthFactor;
+
+    private Interval interval;
 
     public DynamicStack(int growthFactor, int maxOverhead, Result result) {
+        this.interval = Interval.EmptyInterval.getEmptyInterval();
+        this.growthFactor = growthFactor;
+        this.maxOverhead = maxOverhead;
+        this.array = new DynamicArray(growthFactor,maxOverhead);
+        this.result = result;
     }
 
     @Override
     public int size() {
-        return 0;
+
+        return interval.getSize(array.getLength());
     }
 
     @Override
     public void pushBack(int value) {
-
+        if (interval.isEmpty()){
+            interval = new Interval.NonEmptyInterval(0,0);
+        }
+        array.reportUsage(interval, interval.getSize(array.getLength()) + 1);
+        array.set(interval.getSize(array.getLength()), value);
+        interval = new Interval.NonEmptyInterval(interval.getFrom(), interval.getTo() + 1);
+        array.reportArray(result);
     }
 
     @Override
     public int popBack() {
-        return 0;
+        array.reportUsage(interval, interval.getSize(array.getLength()) - 1);
+        interval = new Interval.NonEmptyInterval(interval.getFrom(), interval.getTo() - 1);
+        array.reportArray(result);
+        return array.get(interval.getTo());
     }
 
     @Override
