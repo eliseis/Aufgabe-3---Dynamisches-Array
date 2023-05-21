ce8010393a5c48dbf41b592b0432bde585b0d7b3
elisei <ge94jip@mytum.de>
2023-05-21 20:54:29 +0200

    q

--- src/gad/dynamicarray/RingQueue.java
+++ src/gad/dynamicarray/RingQueue.java
@@ -3,24 +3,54 @@ package gad.dynamicarray;
 public class RingQueue implements Queue {
     private DynamicArray array;
     private Result result;
+    private int maxOverhead;
+    private int growthFactor;
+
+    private Interval interval;
 
     public RingQueue(int growthFactor, int maxOverhead, Result result) {
+        this.interval = Interval.EmptyInterval.getEmptyInterval();
+        this.growthFactor = growthFactor;
+        this.maxOverhead = maxOverhead;
+        this.array = new DynamicArray(growthFactor,maxOverhead);
+        this.result = result;
 
     }
 
     @Override
     public int size() {
-        return 0;
+        return interval.getSize(array.getLength());
     }
 
     @Override
     public void pushBack(int value) {
-
+        if (array.getLength() == 0){
+            interval = array.reportUsage(interval, 1);
+        }
+        else {
+            interval = array.reportUsage(interval, interval.getSize(array.getLength()) + 1);
+        }
+        if (interval.isEmpty()){
+            interval = new Interval.NonEmptyInterval(0,0);
+        }
+        else{
+            interval = new Interval.NonEmptyInterval(interval.getFrom(), interval.getTo() + 1);
+        }
+        array.set(interval.getTo(), value);
+        array.reportArray(result);
     }
 
     @Override
     public int popFront() {
-        return 0;
+        int last = array.get(interval.getTo());
+        if (interval.getSize(array.getLength()) == 1 || interval.getSize(array.getLength()) == 0){
+            interval = array.reportUsage(Interval.EmptyInterval.getEmptyInterval(), 0);
+        }
+        else {
+            interval = array.reportUsage(new Interval.NonEmptyInterval(interval.getFrom(), interval.getTo() - 1), interval.getSize(array.getLength()) - 1);
+        }
+        array.reportArray(result);
+        return last;
     }
 
     @Override
